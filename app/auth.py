49f20cc88dc60cdd1de2bcbd14cb500d092874b6
Mohammad Hasan <mohammad.hasan2004.30.05@gmail.com>
2026-05-23 15:07:39 +0300

    Fix: truncate passwords to 72 bytes for bcrypt compatibility

--- app/auth.py
+++ app/auth.py
@@ -20,10 +20,12 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
+    password = password[:72]
     return pwd_context.hash(password)
 
 
 def verify_password(plain_password, hashed_password):
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 
